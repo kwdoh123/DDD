a47252daa31284f6c7234a2a9469e091dc3faebe
kwdoh123 <64297420+kwdoh123@users.noreply.github.com>
2020-04-25 14:53:39 +0900

    Add files via upload

---
+++ 5252믿9있었다9.c
@@ -0,0 +1,143 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    /*
+    #1
+    int n,a=0,b=0,c=0;
+    scanf("%d", &n);
+    while(1)
+    {
+        if(b%2!=0)
+        {
+            a=a+b;
+            c++;
+        }
+        b++;
+        if(a>=n)
+        {
+            break;
+        }
+    }
+    printf("%d %d", c, a);
+
+    /*#2
+    int i=0,j=0,k=0;
+    scanf("%d", &i);
+    for(j=1;j<=(i*2);j++)
+    {
+        if(j<=i)
+        {
+            for(k=j;k<=i;k++)
+            {
+                printf("*");
+            }
+        }
+        else
+        {
+            for(k=j;k>i;k--)
+            {
+                printf("*");
+            }
+        }
+        printf("\n");
+    }
+
+    #3
+    int a=0,b=0,n=0;
+    scanf("%d", &n);
+    for(a=0;a<n;a++)
+    {
+        for(b=n-b;b>=n;b++)
+        {
+            printf(" ")
+        }
+        for(b=;b>=n;b++)
+        {
+            printf("*")
+        }
+        printf("\n");
+    }*/
+
+
+/*#4
+int i=0,j=0,k=0,a=1,b=0;
+char c='A';
+scanf("%d", &i);
+
+for(j=0;j<i;j++)
+{
+    for(k=j;k<i;k++)
+    {
+        printf("%d", a++);
+    }
+    for(b=0;b<=j;b++)
+    {
+        printf("%c", c++);
+    }
+    printf("\n");
+}
+
+
+
+#5
+int i=0,j=0,k=0;
+    scanf("%d", &i);
+    for(j=1;j<=(i*2);j++)
+    {
+        if(j<=i)
+        {
+            for(k=j;k<=i;k++)
+            {
+                printf("*");
+            }
+        }
+        else
+        {
+            for(k=j;k>i;k--)
+            {
+                printf("*");
+            }
+        }
+        printf("\n");
+    }*/
+
+int a;
+scanf("%d", &a);
+printf("*****\n");
+printf(" ***\n");
+printf("  *\n");
+printf(" ***\n");
+printf("*****\n");
+return 0;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
